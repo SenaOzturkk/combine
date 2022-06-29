52d0a3d272fc1dfab8b653f78315ac886a9cd187
Anıl Eren ÇELİK <anilerencelik@gmail.com>
2022-06-29 22:52:18 +0300

    added safeareaview

--- src/components/screenComponents/EditProfile.js
+++ src/components/screenComponents/EditProfile.js
@@ -6,87 +6,89 @@ import {
   ToastAndroid,
   Image,
   TextInput,
+  SafeAreaView
 } from 'react-native';
 import Ionic from 'react-native-vector-icons/Ionicons';
 
-const EditProfile = ({route, navigation}) => {
-  const {name, accountName, profileImage} = route.params;
+const EditProfile = ({ route, navigation }) => {
+  const { name, accountName, profileImage } = route.params;
   const TostMessage = () => {
     ToastAndroid.show('Edited Sucessfully !', ToastAndroid.SHORT);
   };
   return (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white',
-      }}>
+    <SafeAreaView>
       <View
         style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          padding: 10,
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'white',
         }}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionic name="close-outline" style={{fontSize: 35}} />
-        </TouchableOpacity>
-        <Text style={{fontSize: 16, fontWeight: 'bold'}}>Profili Düzenle</Text>
-        <TouchableOpacity
-          onPress={() => {
-            TostMessage();
-            navigation.goBack();
-          }}>
-          <Ionic name="checkmark" style={{fontSize: 35, color: '#3493D9'}} />
-        </TouchableOpacity>
-      </View>
-      <View style={{padding: 20, alignItems: 'center'}}>
-        <Image
-          source={{uri: profileImage}}
-          style={{width: 80, height: 80, borderRadius: 100}}
-        />
-        <Text
+        <View
           style={{
-            color: 'black',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: 10,
           }}>
-          Profil Fotoğrafını Değiştir
-        </Text>
-      </View>
-      <View style={{padding: 10}}>
-        <View style={{paddingVertical: 10}}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionic name="close-outline" style={{ fontSize: 35 }} />
+          </TouchableOpacity>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Profili Düzenle</Text>
+          <TouchableOpacity
+            onPress={() => {
+              TostMessage();
+              navigation.goBack();
+            }}>
+            <Ionic name="checkmark" style={{ fontSize: 35, color: '#3493D9' }} />
+          </TouchableOpacity>
+        </View>
+        <View style={{ padding: 20, alignItems: 'center' }}>
+          <Image
+            source={{ uri: profileImage }}
+            style={{ width: 80, height: 80, borderRadius: 100 }}
+          />
           <Text
             style={{
-              opacity: 0.5,
+              color: 'black',
             }}>
-            Kullanıcı Adı
+            Profil Fotoğrafını Değiştir
           </Text>
-          <TextInput
-            placeholder="accountname"
-            defaultValue={accountName}
-            style={{
-              fontSize: 16,
-              borderBottomWidth: 1,
-              borderColor: '#CDCDCD',
-            }}
-          />
         </View>
-        <View style={{paddingVertical: 10}}>
-          <Text
-            style={{
-              opacity: 0.5,
-            }}>
-            Şifre
-          </Text>
-          <TextInput
-            style={{
-              fontSize: 16,
-              borderBottomWidth: 1,
-              borderColor: '#CDCDCD',
-            }}
-          />
+        <View style={{ padding: 10 }}>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}>
+              Kullanıcı Adı
+            </Text>
+            <TextInput
+              placeholder="accountname"
+              defaultValue={accountName}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: '#CDCDCD',
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}>
+              Şifre
+            </Text>
+            <TextInput
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: '#CDCDCD',
+              }}
+            />
+          </View>
         </View>
-      </View>
-    </View>
+      </View></SafeAreaView>
   );
 };
 
